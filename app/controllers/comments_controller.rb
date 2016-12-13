9563f13f0e226f085f7046a61acd55b40f2b4ab7
Isaac Toleafoa <isaactoleafoa@gmail.com>
2016-12-12 23:53:45 -0700

    adding comments per user fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,16 @@
 class CommentsController < ApplicationController
 	def create
-		@post = Post.find(params[:id])
-		@comment = @post.comments.create(params[:comment].permit[:name, :body])
-		
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(params[:comment].permit(:name, :body))
+
 		redirect_to post_path(@post)
 	end
 
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+
+		redirect_to post_path(@post)
+	end
 end
